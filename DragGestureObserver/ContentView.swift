a7dd2bd7b8e63e7ef36e46778f419962d8bf4f38
CrystalMinds <info@crystalminds.nl>
2024-03-20 22:36:28 +0100

    Initial commit.

---
+++ DragGestureObserver/ContentView.swift
@@ -0,0 +1,97 @@
+//
+//  ContentView.swift
+//  DragGestureObserver
+//
+//  Created by Mark on 20/03/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @State private var isPressed: Bool = false
+    @State private var displayValue: String = "0"
+
+    var body: some View {
+        VStack {
+            // Simple Demo
+            VStack {
+                Text("Click me")
+                    .fontWeight(.bold)
+                    .foregroundStyle(isPressed ? .blue : .white)
+                    .padding()
+                    .onDragGesture([.downInside, .up]) { proxy in
+                        self.isPressed = (proxy.phase == .downInside)
+                    }
+            }
+            .dragGestureObserver()
+            
+            Divider()
+            
+            // Calculator Button Behavior
+            VStack {
+                CalculatorDisplay(displayValue: $displayValue)
+                HStack {
+                    CalculatorButton(7, displayValue: $displayValue)
+                    CalculatorButton(8, displayValue: $displayValue)
+                    CalculatorButton(9, displayValue: $displayValue)
+                }
+            }
+            .dragGestureObserver()
+        }
+        
+    }
+    
+}
+
+struct CalculatorDisplay: View {
+    @Binding var displayValue: String
+    
+    var body: some View {
+        Text(displayValue)
+            .foregroundStyle(.white)
+            .font(.system(size: 100))
+    }
+}
+
+struct CalculatorButton: View {
+    let number: Int
+    @Binding var displayValue: String
+    
+    @State private var isHighlighted: Bool = false
+    
+    init(_ number: Int, displayValue: Binding<String>) {
+        self.number = number
+        self._displayValue = displayValue
+    }
+    
+    var body: some View {
+        Text("\(number)")
+            .font(.system(size: 40))
+            .foregroundStyle(.white)
+            .frame(width: 70, height: 70)
+            .background(
+                Circle().fill(
+                    !isHighlighted ?
+                        Color(red: 51/255, green: 51/255, blue: 51/255) :
+                        Color(red: 115/255, green: 115/255, blue: 115/255)
+                )
+            )
+            .padding(7)
+            .onDragGesture([.downInside, .up, .upInside, .enter, .exit]) {proxy in
+                if proxy.phase == .downInside || proxy.phase == .enter {
+                    self.isHighlighted = true
+                } else if proxy.phase == .up || proxy.phase == .exit {
+                    self.isHighlighted = false
+                } else if proxy.phase == .upInside {
+                    self.displayValue = "\(number)"
+                }
+            }
+            .animation(.spring, value: isHighlighted)
+    }
+}
+
+#Preview {
+    ContentView()
+        .preferredColorScheme(.dark)
+}
---
+++ DragGestureObserver/DragGestureObserverApp.swift
@@ -0,0 +1,18 @@
+//
+//  DragGestureObserverApp.swift
+//  DragGestureObserver
+//
+//  Created by Mark on 20/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct DragGestureObserverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .preferredColorScheme(.dark)
+        }
+    }
+}
